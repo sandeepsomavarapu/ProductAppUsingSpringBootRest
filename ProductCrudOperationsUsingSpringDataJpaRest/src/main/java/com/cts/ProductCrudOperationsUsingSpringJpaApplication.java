5d2b8e168766823e3c936ba37f283e43f39845d1
sandeepsomavarapu <sandeepsomavarapu535@gmail.com>
2025-04-22 21:34:23 -0700

    day-1 task completed

--- ProductCrudOperationsUsingSpringDataJpaRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
+++ ProductCrudOperationsUsingSpringDataJpaRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCrudOperationsUsingSpringJpaApplication {
 	public static void main(String[] args) {
+		System.out.println("day-1 changes....");
 		SpringApplication.run(ProductCrudOperationsUsingSpringJpaApplication.class, args);
 	}
 }
